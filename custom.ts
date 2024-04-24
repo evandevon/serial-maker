51999d6d4bbfd60f12bc2fb360c35857bb9452bf
evandevon <51048720+evandevon@users.noreply.github.com>
2024-04-25 01:06:08 +1000

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,59 @@
+
+/**
+* Use this file to define custom functions and blocks.
+* Read more at https://makecode.microbit.org/blocks/custom
+*/
+
+enum MyEnum {
+    //% block="one"
+    One,
+    //% block="two"
+    Two
+}
+
+/**
+ * Custom blocks
+ */
+//% color="#AA278D" weight=100
+namespace SerialMaker {
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+    //% block="Average of $value measured $measurements times with $delay (ms) delay"|| icon="\uf080" 
+    export function Averaging(value: number, measurements: number, delay: number): number {
+        let average = 0
+        for (let index = 0; index < measurements; index++) {
+            average += value
+            basic.pause(delay)
+        }
+        average = average / measurements
+        return average;
+    }
+
+
+    //% block
+    export function SendDisplay() {
+        let Display_Grid = "";
+        let Y_Pos = 0;
+        let X_Pos = 0;
+
+        for (let index1 = 0; index1 <= 4; index1++) {
+            Y_Pos = index1;
+            for (let index2 = 0; index2 <= 4; index2++) {
+                X_Pos = index2;
+                if (led.point(X_Pos, Y_Pos)) {
+                    Display_Grid = "" + Display_Grid + "1";
+                } else {
+                    Display_Grid = "" + Display_Grid + "0";
+                }
+            }
+        }
+
+        serial.writeLine("DISPLAY," + Display_Grid);
+        basic.pause(50);
+        return;
+    }
+
+}
+
+
