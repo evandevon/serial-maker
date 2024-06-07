cbffa10c8481583ffe0da06ef8010f80bbb48718
evandevon <51048720+evandevon@users.noreply.github.com>
2024-06-08 00:12:03 +1000

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -329,18 +329,7 @@ namespace SerialMaker {
         basic.pause(20)
     }
 
-    /**
-    * writes to a local file (csv, txt)
-    */
-    //% group="Files"
-    //% color=#b30086
-    //% filename.defl="\\Data Logs\\Data File.txt"
-    //% block="File write $data to bottom of file:$filename"|| icon="\uf080"
-    export function file_add(data: string, filename: string): void {
-        serial.writeLine("FILE_WRITE," + filename + "," + "ADD," + data);
-        basic.pause(20)
-    }
-
+ 
     /**
     * plays a local sound file (mp3, wav)
     */
