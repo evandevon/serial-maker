0676160265e9d7918424d2c07fe5c4a6bde57004
evandevon <51048720+evandevon@users.noreply.github.com>
2024-12-16 00:38:23 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,1 @@
-buttonClicks.onButtonDown(buttonClicks.AorB.A, function () {
-	
-})
-buttonClicks.onButtonHeld(buttonClicks.AorB.A, function () {
-	
-})
-basic.forever(function () {
-    SerialMaker.SendDisplayOverSerial()
-    basic.pause(20)
-})
-basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        led.toggle(randint(0, 4), randint(0, 4))
-    }
-})
+
