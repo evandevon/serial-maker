90220e7b6aedb8eb614ec2f01cd9c8a557117825
evandevon <51048720+evandevon@users.noreply.github.com>
2026-01-09 00:48:55 +1100

    Delete main.ts

--- main.ts
+++
@@ -1,6 +0,0 @@
-input.onButtonPressed(Button.A, function () {
-    SerialMaker.play_sound("Alert 1.wav")
-})
-input.onButtonPressed(Button.B, function () {
-    SerialMaker.Sound_Actions(Sound_Choices.Stop)
-})
