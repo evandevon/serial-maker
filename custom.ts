584b6dff0750337d8850e1ff3f3536663cc7c756
evandevon <51048720+evandevon@users.noreply.github.com>
2026-01-09 23:00:13 +1100

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -1,30 +1,5 @@
-//% color="#AA278D" weight=100
 namespace serialmaker {
 
-    function sendCommand(command: string): void {
-        serial.writeLine(command)
-        basic.pause(10)
-    }
-
-    /* -----------------------------
-       ENUMS
-    ----------------------------- */
-
-    export enum HidState {
-        //% block="enable"
-        Enable,
-        //% block="disable"
-        Disable
-    }
-
-    export enum Axis {
-        //% block="X"
-        X,
-        //% block="Y"
-        Y
-    }
-
-   
 
   
 }
